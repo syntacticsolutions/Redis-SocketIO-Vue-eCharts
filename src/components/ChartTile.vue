80f17a3c3fd1aa31a147474c33c6ee4aeda02d05
Miguel Cabrera <syntacticsolutions@gmail.com>
2017-12-09 11:03:38 -0800

    Added chart resizing for displaying in modal.

--- src/components/ChartTile.vue
+++ src/components/ChartTile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chart-container">
-      <div ref="chart" class="e-chart"></div>
+      <div ref="chart" class="e-chart" @click="showInModal()"></div>
   </div>
 </template>
 
@@ -12,7 +12,33 @@ import _ from 'lodash'
 export default {
     data () {
         return {
-            chart: {}
+            chart: {},
+            options: {
+                title: {
+                text: 'ECharts introductory example'
+                },
+                tooltip: {},
+                xAxis: {
+                    data: ['shirt', 'cardigan', 'chiffon shirt', 'pants', 'heels', 'socks']
+                },
+                yAxis: {},
+                series: [{
+                    name: 'sales',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            }
+        }
+    },
+    methods: {
+        showInModal(){
+            //make modal resize at the same time.
+            this.chart._dom.parentNode.className += ' chart-modal'
+
+            this.chart.resize({
+                width: '800px',
+                height: '600px'
+            })
         }
     },
     mounted () {
@@ -20,35 +46,38 @@ export default {
 
         let chart =  echarts.init(this.$refs.chart)
 
-        chart.setOption({
-            title: {
-            text: 'ECharts introductory example'
-            },
-            tooltip: {},
-            xAxis: {
-                data: ['shirt', 'cardign', 'chiffon shirt', 'pants', 'heels', 'socks']
-            },
-            yAxis: {},
-            series: [{
-                name: 'sales',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        }, true)
-    
+        chart.setOption(this.options, true)
+
+        this.chart = chart;
     }
 }
 </script>
 
 <style lang="scss" scoped>
     .chart-container {
-        background-color: black;
+        background-color: #d9d9d9;
         width:500px;
         height: 350px;
+        transition: left 0.4s cubic-bezier(0.23, 1, 0.32, 1), top 0.4s cubic-bezier(0.23, 1, 0.32, 1);
+        // transition: all 0.4s ease;
     }
 
     .e-chart {
-        width:480px;
-        height:330px;
+        width:450px;
+        height:300px;
+        position: absolute;
+        left: 25px;
+        top:25px;
+    }
+
+    .chart-modal {
+        width: 850px;
+        height: 650px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        margin: auto auto;
     }
 </style>
